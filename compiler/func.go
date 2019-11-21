bd8a589b6cb8119f21ea2980fdbb155f7cacc58f
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-11-21 14:20:12 +0100

    compiler: refactor IR generation
    This is the first commit in a series to refactor the compiler. The
    intention is to make sure every function to be compiled eventually has
    its own IR builder. This will make it much easier to do other
    refactorings in the future:
    
      * Most code won't depend (directly) on the central Compiler object,
        perhaps making it possible to eliminate it in the future. Right now
        it's embedded in the `builder` struct but individual fields from the
        `Compiler` can easily be moved into the `builder` object.
      * Some functions are not directly exposed in Go SSA, they are wrapper
        functions for something. At the moment they are included in the list
        of functions to be compiled with the reachability analysis
        (SimpleDCE) in the ir package, but eventually this reachability
        analys will be removed. At that point, it would be very convenient
        to be able to simply build a function with a new IR builder.
    
    The `compilerContext` struct makes sure that it is not possible for
    `builder` methods to accidentally use global state such as the global IR
    builder. It is a transitional mechanism and may be removed when
    finished.

--- compiler/func.go
+++ compiler/func.go
@@ -31,7 +31,7 @@ const (
 
 // funcImplementation picks an appropriate func value implementation for the
 // target.
-func (c *Compiler) funcImplementation() funcValueImplementation {
+func (c *compilerContext) funcImplementation() funcValueImplementation {
 	// Always pick the switch implementation, as it allows the use of blocking
 	// inside a function that is used as a func value.
 	switch c.Scheduler() {
@@ -109,7 +109,7 @@ func (c *Compiler) decodeFuncValue(funcValue llvm.Value, sig *types.Signature) (
 }
 
 // getFuncType returns the type of a func value given a signature.
-func (c *Compiler) getFuncType(typ *types.Signature) llvm.Type {
+func (c *compilerContext) getFuncType(typ *types.Signature) llvm.Type {
 	switch c.funcImplementation() {
 	case funcValueDoubleword:
 		rawPtr := c.getRawFuncType(typ)
@@ -122,7 +122,7 @@ func (c *Compiler) getFuncType(typ *types.Signature) llvm.Type {
 }
 
 // getRawFuncType returns a LLVM function pointer type for a given signature.
-func (c *Compiler) getRawFuncType(typ *types.Signature) llvm.Type {
+func (c *compilerContext) getRawFuncType(typ *types.Signature) llvm.Type {
 	// Get the return type.
 	var returnType llvm.Type
 	switch typ.Results().Len() {
@@ -152,11 +152,11 @@ func (c *Compiler) getRawFuncType(typ *types.Signature) llvm.Type {
 			// The receiver is not an interface, but a i8* type.
 			recv = c.i8ptrType
 		}
-		paramTypes = append(paramTypes, c.expandFormalParamType(recv)...)
+		paramTypes = append(paramTypes, expandFormalParamType(recv)...)
 	}
 	for i := 0; i < typ.Params().Len(); i++ {
 		subType := c.getLLVMType(typ.Params().At(i).Type())
-		paramTypes = append(paramTypes, c.expandFormalParamType(subType)...)
+		paramTypes = append(paramTypes, expandFormalParamType(subType)...)
 	}
 	// All functions take these parameters at the end.
 	paramTypes = append(paramTypes, c.i8ptrType) // context
